f76c6f5c39e5a819256d6f9e30a6161f48d9c69f
George Li <lzqgeorge@hotmail.com>
2020-12-10 23:59:40 -0800

    Update atoi_iota.c

--- Data_Struct_Implementation/atoi_itoa/atoi_iota.c
+++ Data_Struct_Implementation/atoi_itoa/atoi_iota.c
@@ -55,6 +55,59 @@ char *myitoa(int val, char* str, int base, int size) {
     retur str;
 }
 
+// inline function to swap two numbers
+inline void swap(char *x, char *y) {
+    char t = *x; *x = *y; *y = t;
+}
+ 
+// function to reverse buffer[i..j]
+char* reverse(char *buffer, int i, int j)
+{
+    while (i < j)
+        swap(&buffer[i++], &buffer[j--]);
+ 
+    return buffer;
+}
+ 
+// Iterative function to implement itoa() function in C
+char* itoa(int value, char* buffer, int base)
+{
+    // invalid input
+    if (base < 2 || base > 32)
+        return buffer;
+ 
+    // consider absolute value of number
+    int n = abs(value);
+ 
+    int i = 0;
+    while (n)
+    {
+        int r = n % base;
+ 
+        if (r >= 10) 
+            buffer[i++] = 65 + (r - 10);
+        else
+            buffer[i++] = 48 + r;
+ 
+        n = n / base;
+    }
+ 
+    // if number is 0
+    if (i == 0)
+        buffer[i++] = '0';
+ 
+    // If base is 10 and value is negative, the resulting string 
+    // is preceded with a minus sign (-)
+    // With any other base, value is always considered unsigned
+    if (value < 0 && base == 10)
+        buffer[i++] = '-';
+ 
+    buffer[i] = '\0'; // null terminate string
+ 
+    // reverse the string and return it
+    return reverse(buffer, 0, i - 1);
+}
+
 int main(int argc, char **argv) {
     int test_base10[5] = {0, -10, 12345, -12345, INT_MIN, INT_MAX};
     char buffer[20];

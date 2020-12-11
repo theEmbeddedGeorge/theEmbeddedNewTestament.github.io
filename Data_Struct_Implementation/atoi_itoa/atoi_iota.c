407a338a4da1341259333187addb5382774d56a4
lonelygoddady <lzqgeorge@hotmail.com>
2020-12-10 23:57:06 -0800

    atoi

--- Data_Struct_Implementation/atoi_itoa/atoi_iota.c
+++ Data_Struct_Implementation/atoi_itoa/atoi_iota.c
@@ -3,37 +3,38 @@
 #include <stdio.h>
 #include <limits.h>
 
-char *myitoa(int val, char* str, int base, int size) {
-    int i = 0;
+char *myitoa(int val, char* str, int base) {
+    int i = 0, isNegative = 0;
     char digit;
     char *head, *tail;
     
     if (!str)
         return NULL;
     
-    if (base != 2 || base != 10 || base != 16)
+    if (base != 2 && base != 10 && base != 16)
         return NULL;
 
-    do {
-        // not enough space for conversion
-        if (i == size)
-            return NULL;
+    if (base == 10 && val < 0) {
+        isNegative = 1;
+        val = -val;
+    }
 
+    if (val < 0)
+        val = -val;
+
+    do {
         // if not 16 base, no need to worry about letters
-        if (base != 16)
-            if (base == 10 && val < 0 && i == 0) {
-                *str = '-';
-                i ++;
-            }
-            digit = '0' + val%base;
-        else {
-            digit = (val%base < 10) ? '0' + val%base : 'A' + (val%base - 10);
-        }
+        digit = (val%base < 10) ? '0' + val%base : 'A' + (val%base - 10);
         *(str + i) = digit;
         i++;
         val /= base;
     }
     while (val);
+
+    if (isNegative) {
+        *(str + i) = '-';
+        i ++;
+    }
 
     // NULL terminate
     *(str + i) = '\0';
@@ -50,29 +51,33 @@ char *myitoa(int val, char* str, int base, int size) {
         digit = *head;
         *head = *tail;
         *tail = digit;
+        head ++;
+        tail --;
     } 
 
-    retur str;
+    return str;
 }
 
 int main(int argc, char **argv) {
-    int test_base10[5] = {0, -10, 12345, -12345, INT_MIN, INT_MAX};
+    int i;
     char buffer[20];
-    memset(buffer, 0, 20)
-    for (i=0; i<5; i++) 
-        printf("%s ", myitoa(test_base10[i], buffer, 10, 20)); 
+
+    int test_base10[6] = {0, -10, 12345, -12345, INT_MIN, INT_MAX};
+    memset(buffer, 0, 20);
+    for (i=0; i<6; i++) 
+        printf("%s ", myitoa(test_base10[i], buffer, 10));
+    printf("\n==============\n");  
     
-    int test_base2[5] = {0, -16, 12345, -12345, INT_MIN, INT_MAX};
-    char buffer[20];
-    memset(buffer, 0, 20)
-    for (i=0; i<5; i++) 
-        printf("%s ", myitoa(test_base2[i], buffer, 2, 20));
+    int test_base2[6] = {0, -16, 12345, -12345, INT_MIN, INT_MAX};
+    memset(buffer, 0, 20);
+    for (i=0; i<6; i++) 
+        printf("%s ", myitoa(test_base2[i], buffer, 2));
+    printf("\n==============\n"); 
 
-    int test_base16[5] = {0, -10, 0x1234abcd, -12345, INT_MIN, INT_MAX};
-    char buffer[20];
-    memset(buffer, 0, 20)
-    for (i=0; i<5; i++) 
-        printf("%s ", myitoa(test_base16[i], buffer, 2, 20)); 
-
+    int test_base16[6] = {0, -10, 0x1234abcd, -12345, INT_MIN, INT_MAX};
+    memset(buffer, 0, 20);
+    for (i=0; i<6; i++)
+        printf("%s ", myitoa(test_base16[i], buffer, 16)); 
+    printf("\n==============\n");  
     return 0; 
 }

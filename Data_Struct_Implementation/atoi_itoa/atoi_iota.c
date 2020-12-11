0e98288e36994b6e9258635f71a81a6f4ea5dfea
George Li <lzqgeorge@hotmail.com>
2020-12-10 23:14:59 -0800

    add atoi/itoa implementation

---
+++ Data_Struct_Implementation/atoi_itoa/atoi_iota.c
@@ -0,0 +1,78 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <limits.h>
+
+char *myitoa(int val, char* str, int base, int size) {
+    int i = 0;
+    char digit;
+    char *head, *tail;
+    
+    if (!str)
+        return NULL;
+    
+    if (base != 2 || base != 10 || base != 16)
+        return NULL;
+
+    do {
+        // not enough space for conversion
+        if (i == size)
+            return NULL;
+
+        // if not 16 base, no need to worry about letters
+        if (base != 16)
+            if (base == 10 && val < 0 && i == 0) {
+                *str = '-';
+                i ++;
+            }
+            digit = '0' + val%base;
+        else {
+            digit = (val%base < 10) ? '0' + val%base : 'A' + (val%base - 10);
+        }
+        *(str + i) = digit;
+        i++;
+        val /= base;
+    }
+    while (val);
+
+    // NULL terminate
+    *(str + i) = '\0';
+
+    // if base 10 and negative number, there is a minus sign
+    if (base == 10 && val < 0)
+        head = &str[1];
+    else
+        head = str;
+    
+    tail = &str[i-1];
+
+    while (head < tail) {
+        digit = *head;
+        *head = *tail;
+        *tail = digit;
+    } 
+
+    retur str;
+}
+
+int main(int argc, char **argv) {
+    int test_base10[5] = {0, -10, 12345, -12345, INT_MIN, INT_MAX};
+    char buffer[20];
+    memset(buffer, 0, 20)
+    for (i=0; i<5; i++) 
+        printf("%s ", myitoa(test_base10[i], buffer, 10, 20)); 
+    
+    int test_base2[5] = {0, -16, 12345, -12345, INT_MIN, INT_MAX};
+    char buffer[20];
+    memset(buffer, 0, 20)
+    for (i=0; i<5; i++) 
+        printf("%s ", myitoa(test_base2[i], buffer, 2, 20));
+
+    int test_base16[5] = {0, -10, 0x1234abcd, -12345, INT_MIN, INT_MAX};
+    char buffer[20];
+    memset(buffer, 0, 20)
+    for (i=0; i<5; i++) 
+        printf("%s ", myitoa(test_base16[i], buffer, 2, 20)); 
+
+    return 0; 
+}

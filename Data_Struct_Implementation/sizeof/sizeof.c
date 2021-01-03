05a6de2bd6d4a4fe032bf870f68fd6c61cef0e58
theEmbeddedGeorge <lzqgeorge@hotmail.com>
2021-01-02 22:09:12 -0800

    add sizeof implementation

---
+++ Data_Struct_Implementation/sizeof/sizeof.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+
+#define my_sizeof(type) (char *)(&type+1)-(char*)(&type) 
+
+typedef struct Random_item {
+    char a;
+    int b;
+    char c;
+} Random_item;
+
+typedef struct Random_item2 {
+    char a;
+    int b;
+    char c;
+    char d;
+    uint16_t e;
+} Random_item2;
+
+int main() {
+    Random_item tmp;
+    Random_item2 tmp2;
+
+    printf("my_sizeof the item is %ld sizeof: %ld\n", my_sizeof(tmp), sizeof(tmp));
+    printf("my_sizeof the item is %ld sizeof: %ld\n", my_sizeof(tmp2), sizeof(tmp2));
+
+    return 0;
+}

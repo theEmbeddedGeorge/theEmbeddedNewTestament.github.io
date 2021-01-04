6c54154cd6db9a6e26201790e2abbfaf13538d00
theEmbeddedGeorge <lzqgeorge@hotmail.com>
2021-01-03 17:22:10 -0800

    more sizeof

--- Data_Struct_Implementation/sizeof/sizeof.c
+++ Data_Struct_Implementation/sizeof/sizeof.c
@@ -26,6 +26,13 @@ typedef struct Random_item3 {
     uint16_t f;
 } Random_item3;
 
+void argument_alignment_check( char c1, char c2 ) 
+{ 
+   // Considering downward stack 
+   // (on upward stack the output will be negative) 
+   printf("Displacement %ld\n", (char*)&c2 - (char*)&c1); 
+} 
+
 int main() {
     Random_item tmp;
     Random_item2 tmp2;
@@ -34,6 +41,8 @@ int main() {
     printf("my_sizeof the item is %ld sizeof: %ld\n", my_sizeof(tmp), sizeof(tmp));
     printf("my_sizeof the item is %ld sizeof: %ld\n", my_sizeof(tmp2), sizeof(tmp2));
     printf("my_sizeof the item is %ld sizeof: %ld\n", my_sizeof(tmp3), sizeof(tmp3));
+
+    argument_alignment_check('a', 'b');
 
     return 0;
 }

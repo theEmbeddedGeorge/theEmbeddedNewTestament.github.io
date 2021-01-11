46369d0fb6dbb127b026575a565ae19113d5cf7a
theEmbeddedGeorge <lzqgeorge@hotmail.com>
2021-01-10 23:06:50 -0800

    update fan control app demo

--- Interview/Company/intuitive/fan_hw.c
+++ Interview/Company/intuitive/fan_hw.c
@@ -1,1 +1,37 @@
 #include "fan_hw.h"
+
+#define MMAP(base_addr, offset) {printf("Map physical address %x with offset %x.\n", base_addr, offset);}
+#define UMAP(base_addr) {printf("UMap physical address %x.\n", base_addr);}
+#define READW(ptr, val) {printf("Read a word from a pointer address.\n");}
+#define WRITEW(ptr, val) {printf("Write a word to a pointer address.\n");}
+
+static int general_read_speed(uint32_t* value, Fan_hw* fan_self) {
+    // read register
+    void *ptr;
+
+    MMAP(fan_self->RD_REG, 0x100);
+    READW(ptr, value);
+    UMAP(fan_self->RD_REG);
+
+    return 0;
+}
+
+static int general_set_speed(uint8_t duty_cycle, Fan_hw* fan_self) {
+    // read register
+    void *ptr;
+    uint32_t step;
+    uint32_t value;
+
+    /*
+    * Simple algorithm to convert from duty cycles into PWM counts for 
+    * our general fan 
+    */
+    step = (0xfffffff / 100);
+    value = (uint32_t) duty_cycle * step;
+
+    MMAP(fan_self->WRT_REG, 0x100);
+    WRITEW(ptr, value);
+    UMAP(fan_self->WRT_REG);
+
+    return 0;
+}
--- Interview/Company/intuitive/fan_hw.h
+++ Interview/Company/intuitive/fan_hw.h
@@ -22,14 +22,14 @@ static int general_read_speed(uint32_t* value, Fan_hw* fan_self);
 static int general_set_speed(uint32_t duty_cycle, Fan_hw* fan_self);
 
 Fan_hw general_fan_list[MAX_FAN_NUM] {
-    {"General_vendor", "0xa1", 0xffff8000, 0xffff4000},
-    {"General_vendor", "0xa1", 0xffff8000, 0xffff4000},
-    {"General_vendor", "0xb2", 0xffffa000, 0xffff2000},
-    {"General_vendor", "0xb2", 0xffffa000, 0xffff2000},
-    {"General_vendor", "0xc3", 0xffffe000, 0xffff6000},
-    {"General_vendor", "0xc3", 0xffffe000, 0xffff6000},
-    {"General_vendor", "0xd4", 0xffff8020, 0xffff4020},
-    {"General_vendor", "0xd4", 0xffff8020, 0xffff4020},
-    {"General_vendor", "0xe5", 0xffff8040, 0xffff1000},
-    {"General_vendor", "0xe5", 0xffff8040, 0xffff1000},
+    {"General_vendor", "0xa1", 0xffff8000, 0xffff4000, general_read_speed, general_set_speed},
+    {"General_vendor", "0xa1", 0xffff8000, 0xffff4000, general_read_speed, general_set_speed},
+    {"General_vendor", "0xb2", 0xffffa000, 0xffff2000, general_read_speed, general_set_speed},
+    {"General_vendor", "0xb2", 0xffffa000, 0xffff2000, general_read_speed, general_set_speed},
+    {"General_vendor", "0xc3", 0xffffe000, 0xffff6000, general_read_speed, general_set_speed},
+    {"General_vendor", "0xc3", 0xffffe000, 0xffff6000, general_read_speed, general_set_speed},
+    {"General_vendor", "0xd4", 0xffff8020, 0xffff4020, general_read_speed, general_set_speed},
+    {"General_vendor", "0xd4", 0xffff8020, 0xffff4020, general_read_speed, general_set_speed},
+    {"General_vendor", "0xe5", 0xffff8040, 0xffff1000, general_read_speed, general_set_speed},
+    {"General_vendor", "0xe5", 0xffff8040, 0xffff1000, general_read_speed, general_set_speed},
 }

df8c111703265830b8cae0040d77cc2a93502a71
theEmbeddedGeorge <lzqgeorge@hotmail.com>
2021-02-17 20:41:20 -0800

    add atoi()

--- Data_Struct_Implementation/atoi/README.md
+++ Data_Struct_Implementation/atoi/README.md
@@ -1,21 +1,97 @@
-## Safe Memcpy that Handles Overlapped Src & Dest (Memmove)
-#### ***Usage***
+## Atoi that takes different bases input
+### ***Usage***
 ```
 make
 ./atoi
 ```
 
-#### ***Notes***
+### ***Notes***
 
 
 
-#### ***Improvements***
+### ***Improvements***
 
 
 
-#### ***Code***
-###### itoa.c
+### ***Code***
 ```c
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <limits.h>
 
+int my_atoi(char* num, int size, int base) {
+    int i, ret = 0;
+    int neg = 0;
+    
+    if (!num || size == 0)
+        return ret;
+    
+    // take care of negative sign if any
+    if (num[0] == '-') {
+        neg = 1;
+        i = 1;
+    } else {
+        i = 0;
+    }
+
+    // take care of different bases
+    for (i; i < size; i++) {
+        ret *= base;
+        if (base != 16)
+            ret += num[i] - '0';
+        else { // special handling for Hex numbers
+            if (num[i] >= 'a' && num[i] <= 'f')
+                ret += num[i] - 'a' + 10;
+            else if (num[i] >= 'A' && num[i] <= 'F')
+                ret += num[i] - 'A' + 10;
+            else if (num[i] >= '0' && num[i] <= '9')
+                ret += num[i] - '0';
+        } 
+    }
+
+    return neg ? -ret : ret;
+}
+
+int main(int argc, char **argv) {
+    int ret;
+
+    char* test_base10 = "123456";
+    printf("my: %d expect: %d", my_atoi(test_base10, strlen(test_base10), 10), atoi(test_base10));
+    printf("\n==============\n");  
+    
+    test_base10 = "-123456";
+    printf("my: %d expect: %d ", my_atoi(test_base10, strlen(test_base10), 10), atoi(test_base10));
+    printf("\n==============\n");  
+
+    char *test_base2 = "1000";
+    printf("%d ", my_atoi(test_base2, strlen(test_base2), 2));
+    printf("\n==============\n");
+
+    test_base2 = "111";
+    printf("%d ", my_atoi(test_base2, strlen(test_base2), 2));
+    printf("\n==============\n");
+
+    char *test_base16 = "1a";
+    printf("%d ", my_atoi(test_base16, strlen(test_base16), 16));
+    printf("\n==============\n");
+
+    test_base16 = "100";
+    printf("%d ", my_atoi(test_base16, strlen(test_base16), 16));
+    printf("\n==============\n");
+
+    test_base16 = "ff";
+    printf("%d ", my_atoi(test_base16, strlen(test_base16), 16));
+    printf("\n==============\n");
+
+    test_base16 = "1A";
+    printf("%d ", my_atoi(test_base16, strlen(test_base16), 16));
+    printf("\n==============\n");
+
+    test_base16 = "FF";
+    printf("%d ", my_atoi(test_base16, strlen(test_base16), 16));
+    printf("\n==============\n");
+    return 0; 
+}
 ```
 
--- Data_Struct_Implementation/atoi/atoi.c
+++ Data_Struct_Implementation/atoi/atoi.c
@@ -3,28 +3,76 @@
 #include <stdio.h>
 #include <limits.h>
 
+int my_atoi(char* num, int size, int base) {
+    int i, ret = 0;
+    int neg = 0;
+    
+    if (!num || size == 0)
+        return ret;
+    
+    // take care of negative sign if any
+    if (num[0] == '-') {
+        neg = 1;
+        i = 1;
+    } else {
+        i = 0;
+    }
+
+    // take care of different bases
+    for (i; i < size; i++) {
+        ret *= base;
+        if (base != 16)
+            ret += num[i] - '0';
+        else { // special handling for Hex numbers
+            if (num[i] >= 'a' && num[i] <= 'f')
+                ret += num[i] - 'a' + 10;
+            else if (num[i] >= 'A' && num[i] <= 'F')
+                ret += num[i] - 'A' + 10;
+            else if (num[i] >= '0' && num[i] <= '9')
+                ret += num[i] - '0';
+        } 
+    }
+
+    return neg ? -ret : ret;
+}
 
 int main(int argc, char **argv) {
-    int i;
-    char buffer[64];
-    int buf_s = 64;
+    int ret;
 
-    int test_base10[6] = {0, -10, 12345, -12345, -32768, INT_MAX};
-    memset(buffer, 0, buf_s);
-    for (i=0; i<6; i++) 
-        printf("%s ", itoa(test_base10[i], buffer, 10));
+    char* test_base10 = "123456";
+    printf("my: %d expect: %d", my_atoi(test_base10, strlen(test_base10), 10), atoi(test_base10));
     printf("\n==============\n");  
     
-    int test_base2[6] = {0, -16, 12345, -12345, -32768, INT_MAX};
-    memset(buffer, 0, buf_s);
-    for (i=0; i<6; i++) 
-        printf("%s ", itoa(test_base2[i], buffer, 2));
-    printf("\n==============\n"); 
-
-    int test_base16[6] = {0, -10, 0x1234abcd, -12345, -32768, INT_MAX};
-    memset(buffer, 0, buf_s);
-    for (i=0; i<6; i++)
-        printf("%s ", itoa(test_base16[i], buffer, 16)); 
+    test_base10 = "-123456";
+    printf("my: %d expect: %d ", my_atoi(test_base10, strlen(test_base10), 10), atoi(test_base10));
     printf("\n==============\n");  
+
+    char *test_base2 = "1000";
+    printf("%d ", my_atoi(test_base2, strlen(test_base2), 2));
+    printf("\n==============\n");
+
+    test_base2 = "111";
+    printf("%d ", my_atoi(test_base2, strlen(test_base2), 2));
+    printf("\n==============\n");
+
+    char *test_base16 = "1a";
+    printf("%d ", my_atoi(test_base16, strlen(test_base16), 16));
+    printf("\n==============\n");
+
+    test_base16 = "100";
+    printf("%d ", my_atoi(test_base16, strlen(test_base16), 16));
+    printf("\n==============\n");
+
+    test_base16 = "ff";
+    printf("%d ", my_atoi(test_base16, strlen(test_base16), 16));
+    printf("\n==============\n");
+
+    test_base16 = "1A";
+    printf("%d ", my_atoi(test_base16, strlen(test_base16), 16));
+    printf("\n==============\n");
+
+    test_base16 = "FF";
+    printf("%d ", my_atoi(test_base16, strlen(test_base16), 16));
+    printf("\n==============\n");
     return 0; 
 }

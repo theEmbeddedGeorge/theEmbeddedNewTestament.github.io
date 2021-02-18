9ca600f703d6956133b6e4d2e87971ebb4b50783
theEmbeddedGeorge <lzqgeorge@hotmail.com>
2021-02-17 21:21:06 -0800

    add array of bits

---
+++ Data_Struct_Implementation/bitsArray/bitsArray.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+#define SetBit(A, k) (A[k/32] |= (1 << k%32))
+#define ClearBit(A, k) (A[k/32] &= ~(1 << k%32))
+#define TestBit(A, k) ((A[k/32] & (1 << k%32)))
+
+int main( int argc, char* argv[] )
+{
+   int A[10] = {0};
+   int i;
+
+   for ( i = 0; i < 10; i++ )
+      A[i] = 0;                    // Clear the bit array
+
+   printf("Set bit poistions 100, 200 and 300\n");
+   SetBit( A, 100 );               // Set 3 bits
+   SetBit( A, 200 );
+   SetBit( A, 300 );
+
+   // Check if SetBit() works:
+   for ( i = 0; i < 320; i++ )
+      if ( TestBit(A, i) )
+         printf("Bit %d was set !\n", i);
+
+   printf("\nClear bit poistions 200 \n");
+   ClearBit( A, 200 );
+
+   // Check if ClearBit() works:
+   for ( i = 0; i < 320; i++ )
+      if ( TestBit(A, i) )
+         printf("Bit %d was set !\n", i);
+}
--- Data_Struct_Implementation/bitsArray/bitsArray.md
+++
@@ -1,3 +0,0 @@
-## Bits Array
-
-[Array of bits introduction](http://www.mathcs.emory.edu/~cheung/Courses/255/Syllabus/1-C-intro/bit-array.html)
